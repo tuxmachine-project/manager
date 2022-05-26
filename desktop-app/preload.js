b1aacea2661d8aeb57a07c9b86eb98bc0b2f6635
thatretrodev <thatretrodev@outlook.com>
2022-05-26 17:22:21 -0400

    Basic prototype

---
+++ desktop-app/preload.js
@@ -0,0 +1,19 @@
+const { contextBridge, ipcRenderer } = require('electron');
+const { execFileSync } = require('child_process');
+
+function startGame(name) {
+	const filenames = {
+		"tuxmath": "/usr/games/tuxmath",
+		"tuxpaint": "/usr/bin/tuxpaint",
+		"tuxtype": "/usr/games/tuxtype",
+		"etr": "/usr/games/etr"
+	};
+
+	if (Object.keys(filenames).includes(name)) {
+		execFileSync(filenames[name]);
+	}
+}
+
+contextBridge.exposeInMainWorld("tuxmachine", {
+	startGame
+});
---
+++ web/src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import '@fontsource/roboto';
+import './App.css';
+
+function App() {
+	const startGame = typeof (window as any).tuxmachine == "undefined" ? (name: string) => {} : (window as any).tuxmachine.startGame;
+	
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1 style={{fontSize: "10vw", marginBottom: 0}}>Welcome!</h1>
+				<p>Select a game:</p>
+				
+				<button style={{marginTop: 15}} onClick={() => {startGame("tuxmath");}}>Tux Math</button>
+				<button style={{marginTop: 15}} onClick={() => {startGame("tuxpaint");}}>Tux Paint</button>
+				<button style={{marginTop: 15}} onClick={() => {startGame("tuxtype");}}>Tux Typing</button>
+				<button style={{marginTop: 15}} onClick={() => {startGame("etr");}}>Extreme Tux Racer</button>
+			</header>
+		</div>
+	);
+}
+
+export default App;
